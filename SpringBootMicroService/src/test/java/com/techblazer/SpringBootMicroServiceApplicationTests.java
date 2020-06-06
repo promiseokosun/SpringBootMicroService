a63a982fd994467741e2215372681daf92e61e3a
promise <promiseokosun@gmail.com>
2020-06-06 13:10:57 +0100

    Basic Setup of the App

---
+++ SpringBootMicroService/src/test/java/com/techblazer/SpringBootMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techblazer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
