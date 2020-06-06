cf822e84e1ab4a8d5fb823cab1b7c6c5da003ae7
promise <promiseokosun@gmail.com>
2020-06-06 13:07:41 +0100

    Initial Update on the App

---
+++ SpringBootMicroService/src/main/java/com/techblazer/SpringBootMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techblazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroServiceApplication.class, args);
+	}
+
+}
